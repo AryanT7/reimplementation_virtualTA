4cdbaa9ef78d785c1d4dfa7c6c8e87cc7fe5f65e
ishwarya0103 <ishwaryaanandakrishnan@gmail.com>
2026-05-05 15:39:56 -0400

    Fix API base URL for VCL (use relative /api routing)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,12 +6,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function getApiUrl() {
-  const apiUrl = process.env.NODE_ENV === "production" 
-    ? 'http://3.91.189.197:8000'
-    : 'http://localhost:8000'
-    
-
-  
-  return apiUrl
+export function getApiUrl() {  
+  return ''
 }
